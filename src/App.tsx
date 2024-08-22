fedfe812c091bc59548f31a5c2f6691b77a93a64
HungPN <hungpn@HungPN-D8-M.local>
2024-08-22 11:25:03 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,66 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Header from "./components/Header";
+function App() {
+  return (
+    <>
+      <Header />
+      {/* <section id="hero" className="bg-slideshow section-divider-bottom-1">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="hero-wrapper v-center">
+                <h2 data-animation-direction="fade" data-animation-delay="600">
+                  Save the Date
+                </h2>
+
+                <h1 className="hero-title light">
+                  <span
+                    className="d-block d-sm-inline"
+                    data-animation-direction="from-right"
+                    data-animation-delay="300"
+                  >
+                    Kiến Văn
+                  </span>
+                  <small
+                    className="d-block d-sm-inline"
+                    data-animation-direction="from-top"
+                    data-animation-delay="300"
+                  >
+                    &
+                  </small>
+                  <span
+                    className="d-block d-sm-inline"
+                    data-animation-direction="from-left"
+                    data-animation-delay="300"
+                  >
+                    Việt Hoài
+                  </span>
+                </h1>
+
+                <div
+                  className="hero-subtitle light"
+                  data-animation-direction="fade"
+                  data-animation-delay="1000"
+                >
+                  04 Tháng 4 2024
+                </div>
+
+                <div
+                  data-animation-direction="fade"
+                  data-animation-delay="1000"
+                >
+                  <a href="#rsvp" className="btn btn-light scrollto">
+                    Gửi lời chúc
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section> */}
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,122 @@
+import { useEffect } from "react";
+import "./styles.css";
+const Header = () => {
+  useEffect(() => {
+    const navItems = document.querySelectorAll(".menu-item-link");
+    const sections = document.querySelectorAll("section");
+    const observe = new IntersectionObserver(
+      (item) => {
+        if (item[0].isIntersecting) {
+          item[0] &&
+            navItems.forEach((i: any) => {
+              if (
+                i
+                  .getAttribute("href")
+                  .includes(item[0].target.getAttribute("id"))
+              ) {
+                i.classList.add("active");
+              } else {
+                i.classList.remove("active");
+              }
+            });
+        }
+      },
+      { threshold: 0.5 }
+    );
+    sections.forEach((item) => observe.observe(item));
+    window.addEventListener(
+      "scroll",
+      function (e) {
+        const nav = document.getElementById("navbar") as HTMLElement;
+        if (window.scrollY > 0) {
+          nav.classList.add("sticky");
+        } else {
+          nav.classList.remove("sticky");
+        }
+        const scrollUp = document.getElementById(
+          "scroll-up-btn"
+        ) as HTMLElement;
+        if (window.scrollY > 500) {
+          scrollUp.classList.add("show");
+        } else {
+          scrollUp.classList.remove("show");
+        }
+      },
+
+      { passive: true }
+    );
+  }, []);
+
+  return (
+    <>
+      <div className="scroll-up-btn" id="scroll-up-btn">
+        <a href="#home" style={{ color: "white" }}>
+          {/* <FontAwesomeIcon icon={faAngleUp} /> */}
+        </a>
+      </div>
+      <div className="header">
+        <nav className="header-nav" id="navbar">
+          <div className="max-width">
+            <input type="checkbox" id="check"></input>
+            <label htmlFor="check" className="checkbtn">
+              <i className="fas fa-bars"></i>
+            </label>
+            <label className="brand">
+              <a
+                href="#home"
+                className="nav-logo d-flex align-items-center h-100 brand mt-1"
+              >
+                <h1 style={{ fontSize: 30 }}>
+                  V
+                  <span className="main-color" style={{ margin: "0 5px" }}>
+                    <i className="fa fa-heart"></i>
+                  </span>
+                  H
+                </h1>
+              </a>
+            </label>
+            <ul className="menu mt-2">
+              <li className="menu-item">
+                <a className="menu-item-link active" href="#home">
+                  Cặp Đ ôi
+                </a>
+              </li>
+              <li className="menu-item">
+                <a className="menu-item-link" href="#about">
+                  Chuyện Tình Yêu
+                </a>
+              </li>
+              <li className="menu-item">
+                <a className="menu-item-link" href="#services">
+                  Phù Dâu & Phù Rể
+                </a>
+              </li>
+              <li className="menu-item">
+                <a className="menu-item-link" href="#skills">
+                  Album Hình Cưới
+                </a>
+              </li>
+              <li className="menu-item">
+                <a className="menu-item-link" href="#projects">
+                  Sự kiện cưới
+                </a>
+              </li>
+              <li className="menu-item">
+                <a className="menu-item-link" href="#contact">
+                  Sổ Lưu Bút
+                </a>
+              </li>
+              <li className="menu-item">
+                <a className="menu-item-link" href="#contact">
+                  Mừng Cưới
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </>
+  );
+};
+
+export default Header;
