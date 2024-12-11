1290f5ba88ccafc729551e1b0d632d5d03e830f0
hungpn <phamnanghung.25@gmail.com>
2024-12-11 21:18:49 +0700

    tesst

--- src/services/axiosClient.ts
+++ src/services/axiosClient.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const axiosClient = axios.create({
-  baseURL: "https://hungpn256.click/",
+  // baseURL: "https://hungpn256.click/",
+  baseURL: "https://my-wedding-be.onrender.com/",
 });
